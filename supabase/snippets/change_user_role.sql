6d1002226349ee086dd3dd1f1be25043d08cecd9
Saihaan Syed <saihaansyed2003+macbookair@gmail.com>
2026-03-06 09:45:18 -0500

    seed data

--- supabase/snippets/change_user_role.sql
+++ supabase/snippets/change_user_role.sql
@@ -5,7 +5,7 @@
 with target_user as (
   select id
   from auth.users
-  where email = 'sai@chsolutions.ca'
+  where email = 'sai+admin@chsolutions.ca'
 )
 insert into public.user_roles (user_id, role, assigned_by)
 select
